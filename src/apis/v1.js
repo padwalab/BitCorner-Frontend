e54062fe705e150d1bd444f6db2e05de30bcfd82
Abhijeet Padwal <38448205+padwalab@users.noreply.github.com>
2021-05-26 22:49:58 -0700

    server api call fix

--- src/apis/v1.js
+++ src/apis/v1.js
@@ -1,6 +1,5 @@
-export const SERVER =
-  "http://ec2-18-190-25-33.us-east-2.compute.amazonaws.com:8080";
+export const SERVER = "ec2-18-190-25-33.us-east-2.compute.amazonaws.com:8080";
 
-export const SIGN_IN_API = SERVER + "/api/users";
+export const SIGN_IN_API = "http://" + SERVER + "/api/users";
 
-export const LOG_IN_API = SERVER + "/api/users/validate";
+export const LOG_IN_API = "http://" + SERVER + "/api/users/validate";
